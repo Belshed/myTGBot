cc588fd73300474f4e0163ae390be40f77f400fa
Belshed <Belshed@users.noreply.github.com>
2020-04-11 21:44:52 +0300

    bug fixes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import lxml
 import time
 import logging
 import requests
+import telegram.ext
 from covid import Covid
 from threading import Timer
 from bs4 import BeautifulSoup
@@ -19,6 +20,7 @@ logger = logging.getLogger('Bot Logger')
 
 Token = '1078084297:AAGhNxLhFkhinnZNozIowvp42CxZSrGCLqs'
 updater = Updater(token=Token, use_context=True)
+job_queue = updater.job_queue
 dispatcher = updater.dispatcher
 
 target_url = 'https://стопкоронавирус.рф'
@@ -29,21 +31,21 @@ covid_data = covid.get_data()
 
 translator = Translator()
 
-world_pos = 0
+world_pos = 7
 virusData = ""
 country_list = []
 listt = []
 rus_country_list = []
 
 in_emergancy_case = ['Мир', 'США', 'Испания', 'Италия', 'Германия', 'Франция', 'Иран', 'Великобритания', 'Турция', 'Швейцария', 'Бельгия', 'Нидерланды', 'Канада', 'Австрия', 'Бразилия', 'Португалия', 'Южная Корея', 'Израиль', 'Швеция', 'Россия', 'Норвегия', 'Австралия', 'Ирландия', 'Чехия', 'Чили', 'Индия',
-                    'Дания', 'Польша', 'Румыния', 'Малайзия', 'Пакистан', 'Эквадор', 'Филиппины', 'Япония', 'Люксембург', 'Саудовская Аравия', 'Перу', 'Индонезия', 'Таиланд', 'Сербия', 'Финляндия', 'Мексика', 'ОАЭ', 'Панама', 'Катар', 'Доминиканская Республика', 'Греция', 'Южная Африка', 'Колумбия', 'Исландия',
-                    'Аргентина', 'Алжир', 'Сингапур', 'Египет', 'Украина', 'Хорватия', 'Марокко', 'Эстония', 'Новая Зеландия', 'Ирак', 'Словения', 'Молдова', 'Гонконг', 'Литва', 'Армения', 'Бахрейн', 'Венгрия', 'Алмазная принцесса', 'Беларусь', 'Босния и Герцеговина', 'Кувейт', 'Казахстан', 'Камерун', 'Азербайджан',
-                    'Тунис', 'Северная Македония', 'Болгария', 'Латвия', 'Ливан', 'Словакия', 'Андорра', 'Коста Рика', 'Кипр', 'Узбекистан', 'Уругвай', 'Албания', 'Тайвань', 'Катар', 'Буркина Фасо', 'Куба', 'Иордания', 'Реюньон', 'Оман', 'Нормандские острова', "Кот д'Ивуар", 'Гондурас', 'Сан-Марино', 'Палестина', 'Нигер',
-                    'Вьетнам', 'Маврикий', 'Мальта', 'Нигерия', 'Черногория', 'Сенегал', 'Киргизия', 'Гана', 'Грузия', 'Боливия', 'Фарерские острова', 'Шри Ланка', 'Венесуэла', 'ДРК', 'Кения', 'Мартиника', 'Майотта', 'Остров Мэн', 'Гваделупа', 'Бруней', 'Генуа', 'Бангладеш', 'Камбоджа', 'Парагвай', 'Гибралтар', 'Тринидад и Тобаго',
-                    'Руанда', 'Джибути', 'Мадагаскар', 'Лихтенштейн', 'Монако', 'Французская Гвиана', 'Аруба', 'Гватемала', 'Сальвадор', 'Барбадос', 'Ямайка', 'Того', 'Уганда', 'Мали', 'Конго', 'Эфиопия', 'Макао', 'Французская Полинезия', 'Бермудские острова', 'Каймановы острова', 'Замбия', 'Синт-Маартен', 'Сен-Мартен', 'Гайана', 'Эритрея',
-                    'Багамские острова', 'Бенин', 'Габон', 'Гаити', 'Танзания', 'Мьянма', 'Сирия', 'Ливия', 'Мальдивы', 'Гвинея-Бисау', 'Новая Каледония', 'Ангола', 'Экваториальная Гвинея', 'Намибия', 'Антигуа и Барбуда', 'Доминика', 'Монголия', 'Либерия', 'Фиджи', 'Сент-Люсия', 'Кюрасао', 'Судан', 'Гренада', 'Лаос', 'Гренландия', 'Сейшельские острова',
-                    'Суринам', 'Зимбабве', 'Мозамбик', 'Сент-Китс и Невис', 'Eswatini', 'М.С. Зандам', 'Непал', 'Чад', 'Теркс и Кайкос', 'ЦАР', 'Белиз', 'Кабо Верде', 'Ватикан', 'Сент-Винсент и Гренадины', 'Сомали', 'Ботсвана', 'Мавритания', 'Никарагуа', 'Монсеррат', 'Сен-Барт', 'Сьерра-Леоне', 'Бутан', 'Малави', 'Гамбия', 'Сан-Томе и Принсипи', 'Западная Сахара',
-                    'Ангилья', 'Британские Виргинские острова', 'Бурунди', 'Карибские острова Нидерланды', 'Фолклендские острова', 'Папуа - Новая Гвинея', 'Saint Pierre & Miquelon', 'Южный Судан', 'Восточный Тимор', 'Китай']
+                     'Дания', 'Польша', 'Румыния', 'Малайзия', 'Пакистан', 'Эквадор', 'Филиппины', 'Япония', 'Люксембург', 'Саудовская Аравия', 'Перу', 'Индонезия', 'Таиланд', 'Сербия', 'Финляндия', 'Мексика', 'ОАЭ', 'Панама', 'Катар', 'Доминиканская Республика', 'Греция', 'Южная Африка', 'Колумбия', 'Исландия',
+                     'Аргентина', 'Алжир', 'Сингапур', 'Египет', 'Украина', 'Хорватия', 'Марокко', 'Эстония', 'Новая Зеландия', 'Ирак', 'Словения', 'Молдова', 'Гонконг', 'Литва', 'Армения', 'Бахрейн', 'Венгрия', 'Алмазная принцесса', 'Беларусь', 'Босния и Герцеговина', 'Кувейт', 'Казахстан', 'Камерун', 'Азербайджан',
+                     'Тунис', 'Северная Македония', 'Болгария', 'Латвия', 'Ливан', 'Словакия', 'Андорра', 'Коста Рика', 'Кипр', 'Узбекистан', 'Уругвай', 'Албания', 'Тайвань', 'Катар', 'Буркина Фасо', 'Куба', 'Иордания', 'Реюньон', 'Оман', 'Нормандские острова', "Кот д'Ивуар", 'Гондурас', 'Сан-Марино', 'Палестина', 'Нигер',
+                     'Вьетнам', 'Маврикий', 'Мальта', 'Нигерия', 'Черногория', 'Сенегал', 'Киргизия', 'Гана', 'Грузия', 'Боливия', 'Фарерские острова', 'Шри Ланка', 'Венесуэла', 'ДРК', 'Кения', 'Мартиника', 'Майотта', 'Остров Мэн', 'Гваделупа', 'Бруней', 'Генуа', 'Бангладеш', 'Камбоджа', 'Парагвай', 'Гибралтар', 'Тринидад и Тобаго',
+                     'Руанда', 'Джибути', 'Мадагаскар', 'Лихтенштейн', 'Монако', 'Французская Гвиана', 'Аруба', 'Гватемала', 'Сальвадор', 'Барбадос', 'Ямайка', 'Того', 'Уганда', 'Мали', 'Конго', 'Эфиопия', 'Макао', 'Французская Полинезия', 'Бермудские острова', 'Каймановы острова', 'Замбия', 'Синт-Маартен', 'Сен-Мартен', 'Гайана', 'Эритрея',
+                     'Багамские острова', 'Бенин', 'Габон', 'Гаити', 'Танзания', 'Мьянма', 'Сирия', 'Ливия', 'Мальдивы', 'Гвинея-Бисау', 'Новая Каледония', 'Ангола', 'Экваториальная Гвинея', 'Намибия', 'Антигуа и Барбуда', 'Доминика', 'Монголия', 'Либерия', 'Фиджи', 'Сент-Люсия', 'Кюрасао', 'Судан', 'Гренада', 'Лаос', 'Гренландия', 'Сейшельские острова',
+                     'Суринам', 'Зимбабве', 'Мозамбик', 'Сент-Китс и Невис', 'Eswatini', 'М.С. Зандам', 'Непал', 'Чад', 'Теркс и Кайкос', 'ЦАР', 'Белиз', 'Кабо Верде', 'Ватикан', 'Сент-Винсент и Гренадины', 'Сомали', 'Ботсвана', 'Мавритания', 'Никарагуа', 'Монсеррат', 'Сен-Барт', 'Сьерра-Леоне', 'Бутан', 'Малави', 'Гамбия', 'Сан-Томе и Принсипи', 'Западная Сахара',
+                     'Ангилья', 'Британские Виргинские острова', 'Бурунди', 'Карибские острова Нидерланды', 'Фолклендские острова', 'Папуа - Новая Гвинея', 'Saint Pierre & Miquelon', 'Южный Судан', 'Восточный Тимор', 'Китай']
 
 countdown_dict = {}
 globalVirusData = ""
@@ -64,7 +66,7 @@ worldometers_info = [
                     "deaths"
                     ]
 
-print('Стартуем!\n')
+print('Стартуем!')
 curr_time = time.strftime("%H:%M:%S", time.localtime())
 print(curr_time)
 logger.info(f'Deploying time: {curr_time}')
@@ -82,7 +84,7 @@ def update_country_list():
     translations = translator.translate(country_list, dest='ru')
 
     for translation in translations:
-        if translation.text == 'индейка':
+        if translation.text == 'индюк':
             rus_country_list.append('турция')
         else:
             rus_country_list.append(translation.text.lower())
@@ -92,19 +94,21 @@ def update_country_list():
 
 
 def daemon_covid_update():
-    Timer(600, daemon_covid_update).start()
-    global covid
-    covid = Covid(source="worldometers")
-    global covid_data
-    covid_data = covid.get_data()
-    global response
-    response = requests.get(target_url)
-    update_country_list()
-    if time.strftime("%H", time.localtime()) == '11':
-        pass
-    logger.info("Data update done!")
-    print(country_list)
-    print(rus_country_list)
+    try:
+        Timer(1200, daemon_covid_update).start()
+        global covid
+        covid = Covid(source="worldometers")
+        global covid_data
+        covid_data = covid.get_data()
+        global response
+        response = requests.get(target_url)
+        update_country_list()
+        if time.strftime("%H", time.localtime()) == '11':
+            pass
+        logger.info(f"{country_list[12]} \n {rus_country_list[12]}")
+        logger.info("Data update done!")
+    except:
+        logger.error("Не удалось обновить данные!")
 
 
 def get_data_by_country(country):
@@ -121,8 +125,10 @@ def get_data_by_country(country):
         return f"{country.title()}-Такой страны не найдено"
     logger.info(f" Searching {country.title()}")
     i = 1
+
     while i < (len(countdown_name)):
-        data += str(countdown_name[i]) + str(country_dict[worldometers_info[i - 1]]) + '\n'
+        country_value = str(country_dict[worldometers_info[i - 1]])
+        data += str(countdown_name[i]) + str(country_value) + '\n'
         i += 1
     return data
 
@@ -175,21 +181,25 @@ def get_global_virus_data():
 reply_markup = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text='Инфо по миру'), KeyboardButton(text='Инфо по России')],
-            [KeyboardButton(text='Инфо по стране'),
-             # , KeyboardButton(text='Назад')
+            [KeyboardButton(text='Инфо по стране')  # , KeyboardButton(text='Месседж')
              ]
             ], resize_keyboard=True)
 
 
 def get_inline_keyboard():
     inline_markup = [
-                        [InlineKeyboardButton(text=rus_country_list[country_list.index('russia')].title(), callback_data='russia'), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[1])].title(), callback_data=country_list[1])],
-                        [InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[2])].title(), callback_data=country_list[2]), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[3])].title(), callback_data=country_list[3])],
-                        [InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[4])].title(), callback_data=country_list[4]), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[5])].title(), callback_data=country_list[5])],
-                        [InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[6])].title(), callback_data=country_list[6]), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[7])].title(), callback_data=country_list[7])],
-                        [InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[8])].title(), callback_data=country_list[8]), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[9])].title(), callback_data=country_list[9])]
+                        [InlineKeyboardButton(text=rus_country_list[country_list.index('russia')].title(), callback_data='russia'), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[8])].title(), callback_data=country_list[8])],
+                        [InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[9])].title(), callback_data=country_list[9]), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[10])].title(), callback_data=country_list[10])],
+                        [InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[11])].title(), callback_data=country_list[11]), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[12])].title(), callback_data=country_list[12])],
+                        [InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[13])].title(), callback_data=country_list[13]), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[14])].title(), callback_data=country_list[14])],
+                        [InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[15])].title(), callback_data=country_list[15]), InlineKeyboardButton(text=rus_country_list[country_list.index(country_list[16])].title(), callback_data=country_list[16])]
     ]
     return InlineKeyboardMarkup(inline_markup)
+
+
+def send_message(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id=context.job.context,
+                             text='Сообщение')
 
 
 def inline_keyboard_handler(update, context):
@@ -217,52 +227,53 @@ def start(update, context):
 
 def help(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Итак, это помощь\n ",
+                             text="Если у вас есть предложения по улучшению бота, то напишите мне \n @Belshed",
                              parse_mode='html')
 
 
 def info(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="<b>Выбири что хочешь узнать⬇</b>\n",
+                             text="Данные взяты с источников:\nwww.стопкоронавирус.рф\nwww.worldometers.info/coronavirus/\nЕсли у вас есть предложения по улучшению бота, то напишите мне \n @Belshed\n<b>Выбири что хочешь узнать⬇</b>\n",
                              reply_markup=reply_markup,
                              parse_mode='html')
 
 
 def message(update, context):
-    text = update.message.text
-    if text.lower() == 'инфо':
+    text = update.message.text.lower()
+    if text == 'инфо':
         info(update, context)
 
-    elif text.lower() == 'инфо по миру':
+    elif text == 'инфо по миру':
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=get_global_virus_data())
 
-    elif text.lower() == 'инфо по россии':
+    elif text == 'инфо по россии':
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=parse_stopcorona())
 
-    elif text.lower() == 'инфо по стране':
+    elif text == 'инфо по стране':
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text="Выбери страну ⬇\nИли введи ее самостоятельно",
                                  reply_markup=get_inline_keyboard())
 
-    elif text.lower() == 'ывпреврывапыупупё':
+    elif text == 'ывпреврывапыупупё':
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text="Возвращаемся назад",
                                  reply_markup=ReplyKeyboardRemove())
 
     elif text in country_list:
-        country = text.lower()
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=get_data_by_country(country),
-                                 parse_mode='html')
-
-    elif text.title() in rus_country_list:
         country = text
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=get_data_by_country(country),
                                  parse_mode='html')
 
+    elif text in rus_country_list:
+        country = text
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=get_data_by_country(country),
+                                 parse_mode='html')
+    elif text == 'месседж':
+        job_minute = job_queue.run_once(send_minute, 1, context=update.effective_chat.id)
     else:
         logger.info(f"Message: {text}")
         context.bot.send_message(chat_id=update.effective_chat.id,
@@ -271,6 +282,8 @@ def message(update, context):
 
 
 daemon_covid_update()
+
+# job_minute = job_queue.run_repeating(send_minute, interval=60, first=0)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
